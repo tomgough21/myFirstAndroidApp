5ae64cec581d9f7c62329dda7fbc7b12a1f52832
tomgough21 <tomgough96@gmail.com>
2016-11-01 13:27:24 +0000

    Cheat button added - up to page 100

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +18,7 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mCheatButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -82,12 +84,13 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-
-        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                updateQuestion();
+            public void onClick(View v){
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                Intent i = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);
+                startActivity(i);
             }
         });
 
